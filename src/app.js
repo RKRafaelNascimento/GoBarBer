288d9f38deaeaf82ec1ff05f90101fffd54f79f1
rafael <rafael.nascimento@mutantbr.com>
2019-09-21 11:07:55 -0300

    Adds sequelize and configuration

--- src/app.js
+++ src/app.js
@@ -2,19 +2,19 @@ import express from 'express';
 import router from './routes';
 
 class App {
-  constructor() {
-    this.server = express();
-    this.middleware();
-    this.router();
-  }
+    constructor() {
+        this.server = express();
+        this.middleware();
+        this.router();
+    }
 
-  middleware() {
-    this.server.use(express.json());
-  }
+    middleware() {
+        this.server.use(express.json());
+    }
 
-  router() {
-    this.server.use(router);
-  }
+    router() {
+        this.server.use(router);
+    }
 }
 
 export default new App().server;
