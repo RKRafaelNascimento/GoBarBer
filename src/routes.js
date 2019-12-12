367dd4f90efa7617f41b451cdc341d53717334f7
rafael <rafael.nascimento@mutantbr.com>
2019-12-12 19:16:57 -0300

    Adicionado router de upload de img

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,13 @@
 import { Router } from 'express';
+import multer from 'multer';
 import UserController from './app/controllers/UserController';
 import SessionController from './app/controllers/SessionController';
 import authMiddleware from './app/Middlewares/auth';
 
+import multerConfig from './config/multer';
+
 const routes = new Router();
+const upload = multer(multerConfig);
 
 routes.post('/user', UserController.store);
 routes.post('/session', SessionController.store);
@@ -11,5 +15,9 @@ routes.post('/session', SessionController.store);
 routes.use(authMiddleware);
 
 routes.put('/user', UserController.update);
+
+routes.post('/files', upload.single('file'), (req, res) => {
+    return res.json({ ok: true });
+});
 
 export default routes;
