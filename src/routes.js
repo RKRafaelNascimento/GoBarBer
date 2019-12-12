7da0ca2c47e4ac4485c26274724e41acbdb0d6af
rafael <rafael.nascimento@mutantbr.com>
2019-12-12 19:57:40 -0300

    Adicionado FileControllers

--- src/routes.js
+++ src/routes.js
@@ -2,6 +2,7 @@ import { Router } from 'express';
 import multer from 'multer';
 import UserController from './app/controllers/UserController';
 import SessionController from './app/controllers/SessionController';
+import FileController from './app/controllers/FileController';
 import authMiddleware from './app/Middlewares/auth';
 
 import multerConfig from './config/multer';
@@ -16,8 +17,6 @@ routes.use(authMiddleware);
 
 routes.put('/user', UserController.update);
 
-routes.post('/files', upload.single('file'), (req, res) => {
-    return res.json({ ok: true });
-});
+routes.post('/files', upload.single('file'), FileController.store);
 
 export default routes;
