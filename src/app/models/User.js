ee90839e9c2f96d5cef74c83b83a0f9b85dac429
rafael <rafael.nascimento@mutantbr.com>
2019-12-12 19:58:40 -0300

    Ajuste User models para relacionamento com a file models

--- src/app/models/User.js
+++ src/app/models/User.js
@@ -25,7 +25,11 @@ class User extends Model {
         return this;
     }
 
-     checkPassword(password) {
+    static associate(models) {
+        this.belongsTo(models.File, { foreignKey: 'avatar_id' });
+    }
+
+    checkPassword(password) {
         return bcrypt.compare(password, this.password_hash);
     }
 }
